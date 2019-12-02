b9f0e8d16bdbd04fc6b997077bdfe61057b6c877
TkachukArtiom1 <tkachuk.ar@donnu.edu.ua>
2019-12-02 15:38:46 +0200

    -

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactPlayer from './components/ReactPlayer/ReactPlayer'
+import VideoTag from './components/VideoTag/VideoTag'
+//import VideojsPlayer from './components/VideojsPlayer/VideojsPlayer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+      <React.Fragment>
+        <ReactPlayer
+          chunk_max={6}
+        />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
+/*
+<VideoTag
+  chunk_max={6}
+/>
+*/
---
+++ client-react/src/components/ReactPlayer/ReactPlayer.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+export default class RP extends React.Component {
+  constructor(props) {
+    super(props);
+    this.refStart='http://localhost:3000/assets/chunk';
+    this.refEnd=".webm";
+    this.chunkSize=5;
+    this.mediaSource=new mediaSource();
+    this.mediaSource.addEventListener('sourceopen', this.sourceOpen, { once: true });
+    this.src=URL.createObjectURL(this.MediaSource);
+    this.state = {chunkNum : -1};
+  }
+  getChunkPath(){
+    this.setState({
+      chunkNum:this.state.chunkNum+1
+		});
+    let ref=this.refStart+this.state.chunkNum+this.refEnd;
+    return ref;
+  }
+  sourceOpen() {
+    URL.revokeObjectURL(this.src);
+    const sourceBuffer = this.mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+    sourceBuffer.mode = 'sequence';
+    console.log(this.mediaSource.sourceBuffers);
+    fetch(this.getChunkPath())
+    .then(response => response.arrayBuffer())
+    .then(data => {
+      sourceBuffer.appendBuffer(data);
+    });
+  }
+  fetchNextSegment() {
+    fetch(this.getChunkPath())
+    .then(response => response.arrayBuffer())
+    .then(data => {
+      const sourceBuffer = this.mediaSource.sourceBuffers[0];
+      sourceBuffer.appendBuffer(data);
+    });
+  }
+
+  timeUpdateHandler(){
+    let currentChunk=this.currentTime/this.chunkSize;
+    if(currentChunk>=this.chunkNum){
+      this.fetchNextSegment();
+    }
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <h1
+        style={{
+              justifyContent:"center",
+              display:"flex",
+              margin:"auto",
+            }}
+        >
+          React player
+        </h1>
+        <ReactPlayer
+          style={{
+  							width:"120%",
+  							height:"80%",
+  							display:"flex",
+  							margin:"auto",
+  							backgroundColor:"black",
+  						}}
+            url={this.src}
+            playing={true}
+            muted={true}
+        />
+      </React.Fragment>
+    )
+  }
+}
