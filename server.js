004278fa47450f00a16af454969e7ef53ae70a6a
TkachukArtiom1 <tkachuk.ar@donnu.edu.ua>
2019-12-02 14:18:57 +0200

    -

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 app.use("/assets",express.static(path.join(__dirname,'assets')));
 app.use(express.static(path.join(__dirname, 'clientClearJS')));
-app.use(express.static(path.join(__dirname, 'client-react/build')));
+//app.use(express.static(path.join(__dirname, 'client-react/build')));
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
 })
